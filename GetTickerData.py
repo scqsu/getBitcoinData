7d2e834e78e4290a269a6fc04da0569f32ff6acf
suski_shu <shuciqi@gmail.com>
2020-05-04 00:01:06 +0800

    完成websocket连接

---
+++ GetTickerData.py
@@ -0,0 +1,40 @@
+import asyncio
+import logging
+import gzip
+from io import BytesIO
+from datetime import datetime
+from aiowebsocket.converses import AioWebSocket
+import json
+
+async def startup(uri) :
+    async with AioWebSocket(remote) as aws :
+        converse = aws.manipulator
+        while True:
+            rec = await converse.receive()
+            buff = BytesIO(rec)
+            f = gzip.GzipFile(fileobj=buff)
+            res = f.read().decode('utf-8')
+            rj = json.loads(res)
+            backmsg = '{"pong":' + str(rj['ping']) + '}'
+            if 'ping' in rj :
+                await converse.send(backmsg)
+            print(res, backmsg)
+
+    # async with AioWebSocket(uri) as aws :
+    #     converse = aws.manipulator
+    #     message =  '{"action":"subscribe","args":["QuoteBin5m:14"]}'
+    #     await converse.send(message)
+    #     print('{time}-Client send: {message}'.format(time=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), message=message))
+    #     while True:
+    #         mes = await converse.receive()
+    #         print('{time}-Client receive: {rec}'.format(time=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), rec=mes))
+
+if __name__ == '__main__' :
+    logging.basicConfig(level=logging.DEBUG)
+    
+    remote = 'wss://api.huobi.pro/ws'
+
+    try:
+        asyncio.get_event_loop().run_until_complete(startup(remote))
+    except KeyboardInterrupt as exc :
+        logging.info('Quit.')
